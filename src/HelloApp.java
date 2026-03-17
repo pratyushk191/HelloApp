a878930a84a10af4169cc71c55ff80b13bf19c14
pratyushk191 <pratyushk191@gmail.com>
2026-03-17 23:20:18 +0530

    coded uc6

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,20 +1,23 @@
 public class HelloApp {
+
     public static void main(String[] args) {
-        String name;
+
+        // Case 1: No arguments
         if (args.length == 0) {
-            name = "World";
-        } else {
-            StringBuilder nameBuilder = new StringBuilder();
-            boolean first = true;
-            for (String arg : args) {
-                if (!first) {
-                    nameBuilder.append(", ");
-                }
-                nameBuilder.append(arg);
-                first = false;
-            }
-            name = nameBuilder.toString();
+            System.out.println("Hello, World!");
+            return;
         }
-        System.out.println("Hello, " + name + "!");
+
+        // Case 2: Build names
+        StringBuilder nameBuilder = new StringBuilder();
+
+        for (String name : args) {
+            nameBuilder.append(name).append(", ");
+        }
+
+        // Remove trailing ", "
+        String names = nameBuilder.substring(0, nameBuilder.length() - 2);
+
+        System.out.println("Hello, " + names + "!");
     }
 }
